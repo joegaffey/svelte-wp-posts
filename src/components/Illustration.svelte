5ee7505f5bcbda41026d0f71716f89f0103f84ba
Glitch (glitch-hello-react) <none>
2021-04-02 16:17:06 +0000

    🍏🏌 Checkpoint ./src/components/Illustration.svelte:617503/170

--- src/components/Illustration.svelte
+++ src/components/Illustration.svelte
@@ -1,8 +1,19 @@
 <script>
   import { spring } from 'svelte/motion';
+  
+  let coords = spring({ x: 50, y: 50 }, {
+		stiffness: 0.1,
+		damping: 0.25
+	});
+
+	let size = spring(10);
+  
 </script>
 
-<img src="https://glitch-hello-website.glitch.me/public/illustration.svg" />
+<img cx={$coords.x} 
+     cy={$coords.y} 
+     r={$size} 
+     src="https://glitch-hello-website.glitch.me/public/illustration.svg" />
 
 <style>
 img {
