e5b0c1b3eb598a571a243387e0df71569e518e91
Glitch (glitch-hello-react) <none>
2021-04-02 16:29:06 +0000

    🌝🌍 Checkpoint ./src/components/Illustration.svelte:617503/2876

--- src/components/Illustration.svelte
+++ src/components/Illustration.svelte
@@ -1,20 +1,15 @@
 <script>
   import { spring } from 'svelte/motion';
   
-  let coords = spring({ x: 50, y: 50 }, {
-		stiffness: 0.1,
-		damping: 0.25
-	});
-
-	let size = spring(10);
-  
+  const transformSpring = spring(0, { stiffness: 0.2, damping: 0.1 });
+  const toggleTransform = () => transformSpring.update(val => (val ? 0 : 500));
 </script>
 
-<img cx={$coords.x} 
-     cy={$coords.y} 
-     r={$size} 
-     src="https://glitch-hello-website.glitch.me/public/illustration.svg" />
-
+<img
+     src="https://glitch-hello-website.glitch.me/public/illustration.svg" 
+     style="transform: translateX({$transformSpring}px)"
+     on:hover={toggleTransform}
+/>
 <style>
 img {
   max-width: 100%;
